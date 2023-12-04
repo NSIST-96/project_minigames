e2410da09859549c5d254715ac714d8946bcb22d
NSIST-96 <nsist996@yandex.ru>
2023-12-04 08:52:17 +0300

    Add cowclicker template page and backend functions

--- app.py
+++ app.py
@@ -4,5 +4,18 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'пф'
 
 menu = [{"name": "Домашняя страница", "url": "home.html"},
-        {"name": "CowClicker",        "url": "cowclicker.html"}]
+        {"name": "CowClicker",        "url": "cowclicker.html"},
+        {"name": "Отзыв",             "url": "feedback.html"}]
 
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template('home.html', menu=menu) #все шаблоны по умолчанию берутся из templates/..
+
+@app.route("/cowclicker")
+def cowclicker():
+    return render_template('cowclicker.html', menu=menu)
+
+@app.route("/feedback")
+def feedback():
+    return render_template('feedback.html', menu=menu)
