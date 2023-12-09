985b16e01067a0fca2f92ad0b830e8ad233036ee
NSIST-96 <nsist996@yandex.ru>
2023-12-09 21:46:24 +0300

    Trying to  add snake game

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, flash, session, redirect, url_for, abort #Импортируем фласк и рендер шаблонов
-import database
+import database.database
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'пф'
